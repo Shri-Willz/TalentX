6950efb535ed4de9ae15171eac280caa2755ca30
Shrivathsan S <shriwillz@gmail.com>
2025-10-11 20:06:09 +0530

    cleanUp

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import  SignUpForm from "@/components/SignUpfrom";
+import { SignUpForm } from "@/components/sign-up-form";
 
-export default function SignUp() {
+export default function Page() {
   return (
-    <div>
-      <SignUpForm />
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignUpForm />
+      </div>
     </div>
-  )
+  );
 }
