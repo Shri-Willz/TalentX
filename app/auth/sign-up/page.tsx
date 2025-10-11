aa9d90761eabfc677059cb3ef30b35738d4bdb11
Shrivathsan S <shriwillz@gmail.com>
2025-10-11 18:29:52 +0530

    clenup

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,11 +1,10 @@
-import { SignUpForm } from "@/components/sign-up-form";
+import React from "react";
+import  SignUpForm from "@/components/SignUpfrom";
 
-export default function Page() {
+export default function SignUp() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <SignUpForm />
-      </div>
+    <div>
+      <SignUpForm />
     </div>
-  );
+  )
 }
---
+++ components/SignUpfrom.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+type FromData = {
+    name : string,
+    email : string,
+    password : string
+}
+
+
+export default function SignUpform () {
+    return(
+
+    );
+}
