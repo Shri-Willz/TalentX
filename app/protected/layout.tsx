d595653e40c77004dbf9ef0ff3e29caf799c762b
Shrivathsan S <shriwillz@gmail.com>
2025-10-22 10:08:59 +0530

    pprotected route

---
+++ app/protected/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "TalentX",
+  description: "A modern web app built with Next.js 15 + Tailwind CSS v4",
+};
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning className="bg-background">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
